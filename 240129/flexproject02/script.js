f2d50a4bcc148189725773bdb9c64af872cd86ce
JJleem <leemjaejun@gmail.com>
2024-01-29 18:28:37 +0900

    240129

---
+++ 240129/flexproject02/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".menu-toggle-btn");
+
+const gnb = document.querySelector(".gnb");
+
+btn.addEventListener("click", () => {
+  gnb.classList.toggle("active");
+});
